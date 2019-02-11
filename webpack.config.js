7062909f2d2514449772d3faf123b471980a8008
James Edmonds <james@deckchairlabs.com>
2019-02-11 15:09:36 +1030

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -18,25 +18,10 @@ Encore.setOutputPath(`web/app/themes/${themeName}/dist/`)
    */
   .addEntry('main', `./web/app/themes/${themeName}/javascript/main.js`)
   .addStyleEntry('app', `./web/app/themes/${themeName}/css/app.css`)
-  //.addEntry('page1', './assets/js/page1.js')
-  //.addEntry('page2', './assets/js/page2.js')
 
-  // will require an extra script tag for runtime.js
-  // but, you probably want this, unless you're building a single-page app
   .enableSingleRuntimeChunk()
-
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
-  // enables hashed filenames (e.g. app.abc123.css)
   .enableVersioning(Encore.isProduction())
-
-// uncomment if you use TypeScript
-//.enableTypeScriptLoader()
-
-// uncomment if you use Sass/SCSS files
-//.enableSassLoader()
-
-// uncomment if you're having problems with a jQuery plugin
-//.autoProvidejQuery()
 
 module.exports = Encore.getWebpackConfig()
