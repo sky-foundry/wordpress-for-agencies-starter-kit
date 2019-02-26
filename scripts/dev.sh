9a97932652fed0c6ea110e8f5f08110a1d2e8065
James Edmonds <james@deckchairlabs.com>
2019-02-26 12:17:24 +1030

    use bash script to capture interrupt/kill

---
+++ scripts/dev.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export LOCAL_IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}')
+
+handle_signal () {
+    docker-compose stop
+}
+
+_trap () {
+  for signal in "$@"
+  do
+    trap "handle_signal" "$signal"
+  done
+}
+
+_trap INT KILL STOP
+
+docker-compose up -d && yarn run encore dev-server --watch --host $LOCAL_IP --disable-host-check;
